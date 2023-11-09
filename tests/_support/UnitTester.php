4a31227aa31d797ba84b7e20f922a489953e6be3
Rockylars <rockylars@hotmail.com>
2023-11-09 22:18:51 +0100

    Fixed the tests

--- tests/_support/UnitTester.php
+++ tests/_support/UnitTester.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Rocky\Faker\Tests;
+namespace Rocky\Faker\Tests\Support;
 
 use Codeception\Actor;
+use Rocky\Faker\Tests\Support\_generated\UnitTesterActions;
 
 final class UnitTester extends Actor
 {
