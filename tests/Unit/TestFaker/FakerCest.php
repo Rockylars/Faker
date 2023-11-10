9417a753bed6795853828d1165f99f22f3d93b61
Rockylars <rockylars@hotmail.com>
2023-11-10 20:53:39 +0100

    Added tests for Faker

--- tests/Unit/TestFaker/FakerCest.php
+++ tests/Unit/TestFaker/FakerCest.php
@@ -8,7 +8,9 @@ use Exception;
 use Rocky\Faker\Faker;
 use Rocky\Faker\Tests\Fake\NotFakeButNeededForExample\User\DeleteUserService;
 use Rocky\Faker\Tests\Fake\NotFakeButNeededForExample\User\User;
+use Rocky\Faker\Tests\Fake\NotFakeButNeededForExample\User\UserWatcherService;
 use Rocky\Faker\Tests\Fake\Shared\FakeLogger;
+use Rocky\Faker\Tests\Fake\User\FakeUserChecker;
 use Rocky\Faker\Tests\Fake\User\FakeUserRepository;
 use Rocky\Faker\Tests\Support\UnitTester;
 use RuntimeException;
@@ -18,12 +20,18 @@ final class FakerCest
     private DeleteUserService $deleteUserService;
     private FakeLogger $fakeLogger;
     private FakeUserRepository $fakeUserRepository;
+    private UserWatcherService $userWatcherService;
+    private FakeUserChecker $fakeUserChecker;
 
     public function _before(UnitTester $tester): void
     {
         $this->deleteUserService = new DeleteUserService(
             $this->fakeLogger = new FakeLogger(),
             $this->fakeUserRepository = new FakeUserRepository()
+        );
+        $this->userWatcherService = new UserWatcherService(
+            $this->fakeUserChecker = new FakeUserChecker(),
+            $this->fakeUserRepository
         );
     }
 
@@ -56,7 +64,7 @@ final class FakerCest
         );
     }
 
-    public function returnOrThrowWillReturnAndThrow(UnitTester $tester): void
+    public function returnOrThrowAndVoidOrThrowWillReturnAndVoidAndThrowMultipleResponses(UnitTester $tester): void
     {
         $this->fakeUserRepository->setResponsesFor(FakeUserRepository::FUNCTION_GET_USER_BY_ID, [
             [Faker::ACTION_RETURN => self::getExampleUser()],
@@ -90,13 +98,205 @@ final class FakerCest
         $this->deleteUserService->deleteUser(2);
     }
 
-    /** @skip */
     public function returnOrThrowWillReturnNullValues(UnitTester $tester): void
     {
-        // TODO: Add test for returning null. Requires a new service and new tests for that service.
+        $this->fakeUserChecker->setResponsesFor(FakeUserChecker::FUNCTION_TEST_METHOD_THAT_MIGHT_RETURN_NULL, [
+            [Faker::ACTION_RETURN => null],
+        ]);
+        $this->fakeUserRepository->setResponsesFor(FakeUserRepository::FUNCTION_UPDATE_LAST_LOGIN, [
+            [Faker::ACTION_VOID => null],
+        ]);
+
+        // This assert is not needed, but it is nice to see it come all the way out.
+        $tester->assertNull($this->userWatcherService->userObserved(7));
     }
 
-    // TODO: Add tests for all the call retrievals.
+    public function voidOrThrowFailsWhenNoResponsesAreSet(UnitTester $tester): void
+    {
+        $this->fakeUserRepository->setResponsesFor(FakeUserRepository::FUNCTION_GET_USER_BY_ID, [
+            [Faker::ACTION_RETURN => self::getExampleUser()],
+        ]);
+
+        $tester->expectThrowable(
+            new Exception('No responses defined for Rocky\Faker\Tests\Fake\User\FakeUserRepository::deleteUser, add them to the setResponsesFor'),
+            function (): void {
+                $this->deleteUserService->deleteUser(2);
+            }
+        );
+    }
+
+    public function voidOrThrowFailsWhenNotEnoughResponsesAreSet(UnitTester $tester): void
+    {
+        $this->fakeUserRepository->setResponsesFor(FakeUserRepository::FUNCTION_GET_USER_BY_ID, [
+            [Faker::ACTION_RETURN => self::getExampleUser()],
+            [Faker::ACTION_RETURN => self::getExampleUser()],
+        ]);
+        $this->fakeUserRepository->setResponsesFor(FakeUserRepository::FUNCTION_DELETE_USER, [
+            [Faker::ACTION_VOID => null],
+        ]);
+
+        $this->deleteUserService->deleteUser(2);
+
+        $tester->expectThrowable(
+            new Exception('Not enough responses defined for Rocky\Faker\Tests\Fake\User\FakeUserRepository::deleteUser, add more to the setResponsesFor'),
+            function (): void {
+                $this->deleteUserService->deleteUser(2);
+            }
+        );
+    }
+
+    public function getCallsToWillReturnCallsForIndividualMethods(UnitTester $tester): void
+    {
+        $this->returnOrThrowAndVoidOrThrowWillReturnAndVoidAndThrowMultipleResponses($tester);
+
+        $tester->assertSame(
+            [
+            ],
+            $this->fakeUserRepository->getCallsTo(FakeUserRepository::FUNCTION_UPDATE_LAST_LOGIN)
+        );
+        $tester->assertSame(
+            [
+                [
+                    'userId' => 2
+                ],
+                [
+                    'userId' => 2
+                ],
+                [
+                    'userId' => 2
+                ],
+                [
+                    'userId' => 2
+                ],
+            ],
+            $this->fakeUserRepository->getCallsTo(FakeUserRepository::FUNCTION_GET_USER_BY_ID)
+        );
+    }
+
+    public function getAllCallsInStyleOrderedWillReturnCallsInChronologicalOrder(UnitTester $tester): void
+    {
+        $this->returnOrThrowAndVoidOrThrowWillReturnAndVoidAndThrowMultipleResponses($tester);
+
+        $tester->assertSame(
+            [
+                FakeUserRepository::FUNCTION_GET_USER_BY_ID => [
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                ],
+                FakeUserRepository::FUNCTION_DELETE_USER => [
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                ],
+
+            ],
+            $this->fakeUserRepository->getAllCallsInStyleOrdered()
+        );
+    }
+
+    public function getAllCallsInStyleSortedWillReturnCallsInAlphabeticalOrder(UnitTester $tester): void
+    {
+        $this->returnOrThrowAndVoidOrThrowWillReturnAndVoidAndThrowMultipleResponses($tester);
+
+        $tester->assertSame(
+            [
+                FakeUserRepository::FUNCTION_DELETE_USER => [
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                ],
+                FakeUserRepository::FUNCTION_GET_USER_BY_ID => [
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                ],
+            ],
+            $this->fakeUserRepository->getAllCallsInStyleSorted()
+        );
+    }
+
+    public function getAllCallsInStyleOrderedExceptWillReturnCallsInChronologicalOrder(UnitTester $tester): void
+    {
+        $this->returnOrThrowAndVoidOrThrowWillReturnAndVoidAndThrowMultipleResponses($tester);
+
+        $tester->assertSame(
+            [
+                FakeUserRepository::FUNCTION_GET_USER_BY_ID => [
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                ],
+            ],
+            // TODO: This should get a service with 3+ calls to then erase some of them.
+            $this->fakeUserRepository->getAllCallsInStyleOrderedExcept([FakeUserRepository::FUNCTION_DELETE_USER])
+        );
+    }
+
+    public function getAllCallsInStyleSortedExceptWillReturnCallsInAlphabeticalOrder(UnitTester $tester): void
+    {
+        $this->returnOrThrowAndVoidOrThrowWillReturnAndVoidAndThrowMultipleResponses($tester);
+
+        $tester->assertSame(
+            [
+                FakeUserRepository::FUNCTION_GET_USER_BY_ID => [
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                    [
+                        'userId' => 2
+                    ],
+                ],
+            ],
+            // TODO: This should get a service with 3+ calls to then erase some of them.
+            $this->fakeUserRepository->getAllCallsInStyleSortedExcept([FakeUserRepository::FUNCTION_DELETE_USER])
+        );
+    }
 
     private static function getExampleUser(): User
     {
