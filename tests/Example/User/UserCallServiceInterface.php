6761c8a50ec120f2d5bbf9219c797a085325cdf1
Rockylars <rockylars@hotmail.com>
2024-05-05 10:02:13 +0200

    Added the tests for function

--- tests/Example/User/UserCallServiceInterface.php
+++ tests/Example/User/UserCallServiceInterface.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Rocky\Faker\Tests\Example\User;
 
-interface UserServiceInterface
+interface UserCallServiceInterface
 {
     public function methodThatRuns(User $user): void;
 
     public function methodThatUpdates(User $user): void;
+
+    public function methodThatUpdatesAndReturns(User $user): User;
 
     public function methodThatChecks(User $user): void;
 }
